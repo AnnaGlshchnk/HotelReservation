708f818078acbdd6642b4b249ff9467ba12d6138
AnnaGlshchnk <anna.glushchenko15@mail.ru>
2018-10-21 01:48:09 +0300

    HotelReservation

---
+++ src/main/java/com.anna/db/db.sql
@@ -0,0 +1,38 @@
+DROP TABLE IF EXISTS hotel;
+CREATE TABLE hotel (
+  hotel_id            INT              NOT NULL AUTO_INCREMENT,
+  hotel               VARCHAR(255)     NOT NULL UNIQUE,
+  room_id             INT              NOT NULL,
+  PRIMARY KEY (hotel_id),
+  FOREIGN KEY (room_id) REFERENCES rooms(room_id)  ON DELETE CASCADE
+);
+
+DROP TABLE IF EXISTS rooms;
+CREATE TABLE rooms (
+  room_id      INT              NOT NULL AUTO_INCREMENT,
+  price        INT              NOT NULL,
+  hotel_id     INT              NOT NULL,
+  reserv_id    INT              NOT NULL,
+  PRIMARY KEY (room_id),
+  FOREIGN KEY (reserv_id) REFERENCES reservation(reserv_id)  ON DELETE CASCADE
+);
+
+DROP TABLE IF EXISTS reservation;
+CREATE TABLE reservation (
+  reserv_id       INT            NOT NULL AUTO_INCREMENT,
+  start_reserv    DATE           NOT NULL,
+  end_reserv      DATE           NOT NULL,
+  room_id         INT            NOT NULL,
+  guest_id        INT            NOT NULL
+  PRIMARY KEY (reserv_id),
+  FOREIGN KEY (guest_id) REFERENCES guests(guest_id)  ON DELETE CASCADE,
+);
+
+DROP TABLE IF EXISTS guests;
+CREATE TABLE guests (
+  guest_id       INT            NOT NULL AUTO_INCREMENT,
+  first_name     VARCHAR(255)   NOT NULL,
+  surname        VARCHAR(255)   NOT NULL,
+  PRIMARY KEY (guest_id);
+
+
---
+++ src/main/java/com.anna/db/test_db.sql
@@ -0,0 +1,23 @@
+INSERT INTO hotel (hotel_id, hotel, room_id)
+VALUES (1, 'Grand', 1),
+       (2, 'Raffles', 2),
+       (3, 'Burj al Arab', 3),
+       (4, 'Martinez', 4);
+
+INSERT INTO rooms (room_id, price, hotel_id, reserv_id)
+VALUES (1,  200, 1, 1),
+       (2,  300, 2, 2),
+       (3,  100, 3, 3),
+       (4,  100, 4, 4);
+
+INSERT INTO guests (guest_id, first_name, surname, reserv_id)
+VALUES (1, 'Sam', 'Smit', 1),
+       (2, 'Elisa', 'Petloy', 2),
+       (3, 'Merry', 'Li', 3),
+       (4, 'Jack', 'Jons', 4);
+
+INSERT INTO reservation (reserv_id, start_reserv, end_reserv, room_id)
+VALUES (1, 2018-09-1,  2018-09-6, 1),
+       (2, 2018-09-15,  2018-09-18, 3),
+       (3, 2018-08-1,  2018-08-6, 2),
+       (4, 2018-07-1,  2018-07-15, 4);
