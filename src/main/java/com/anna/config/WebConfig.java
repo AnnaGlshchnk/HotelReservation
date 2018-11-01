b2e0513d4b8f7ae6e23935d07209018a8a0c7024
AnnaGlshchnk <anna.glushchenko15@mail.ru>
2018-11-02 00:09:28 +0300

    HotelReservation

--- src/main/java/com/anna/config/WebConfig.java
+++ src/main/java/com/anna/config/WebConfig.java
@@ -1,6 +1,7 @@
 package com.anna.config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
@@ -10,6 +11,7 @@ import org.springframework.web.servlet.view.freemarker.FreeMarkerViewResolver;
 
 @Configuration
 @EnableWebMvc
+@ComponentScan(basePackages = "com.anna.controller")
 public class WebConfig extends WebMvcConfigurerAdapter {
 
 @Bean
@@ -17,6 +19,7 @@ public class WebConfig extends WebMvcConfigurerAdapter {
     FreeMarkerViewResolver freeMarkerViewResolver = new FreeMarkerViewResolver();
     freeMarkerViewResolver.setOrder(1);
     freeMarkerViewResolver.setSuffix(".ftl");
+    freeMarkerViewResolver.setPrefix("");
     return freeMarkerViewResolver;
     }
 
