f56e4b7e6a3e4d39c0ad023da19cca4cd1e7ec79
AnnaGlshchnk <anna.glushchenko15@mail.ru>
2019-02-19 13:20:00 +0300

    add aspect

---
+++ aspect/src/main/java/com/anna/aspect/Logger.java
@@ -0,0 +1,33 @@
+package com.anna.aspect;
+
+import org.apache.logging.log4j.LogManager;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
+
+@Aspect
+@Component
+public class Logger {
+    private static final org.apache.logging.log4j.Logger LOGGER = LogManager.getLogger(Logger.class.getName());
+
+    @Pointcut("execution(* com.anna.*.*.*(..))")
+    private void ServiceMethod() {
+    }
+
+    @Around(value = "ServiceMethod()")
+    public Object logWebServiceCall(ProceedingJoinPoint joinPoint) throws Throwable {
+        String className = joinPoint.getSignature().getDeclaringTypeName();
+        String methodName = joinPoint.getSignature().getName();
+        Object[] methodArgs = joinPoint.getArgs();
+        Thread curThread = Thread.currentThread();
+        LOGGER.info("From Class " + className + " method " + methodName + " was called on thread " + curThread.getName() + " with args " + Arrays.toString(methodArgs));
+        Object result = joinPoint.proceed();
+        LOGGER.info("From Class " + className + " method " + methodName + " returned value = " + result + " on thread " + curThread.getName());
+
+        return result;
+    }
+}
