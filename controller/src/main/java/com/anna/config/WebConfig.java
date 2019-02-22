fbb87bd8f42ae704cf77f278d3761ab628648408
AnnaGlshchnk <anna.glushchenko15@mail.ru>
2019-02-22 17:50:52 +0300

    add swagger

--- controller/src/main/java/com/anna/config/WebConfig.java
+++ controller/src/main/java/com/anna/config/WebConfig.java
@@ -5,11 +5,21 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.context.annotation.Import;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
 @ComponentScan(basePackages = {"com.anna.dao", "com.anna.service", "com.anna.aspect", "com.anna.controller"})
 @EnableWebMvc
-@Import({DaoConfig.class})
+@Import({DaoConfig.class,  SwaggerConfig.class})
 @EnableAspectJAutoProxy
-public class WebConfig {
+public class WebConfig extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("swagger-ui.html")
+                .addResourceLocations("classpath:/META-INF/resources/");
+        registry.addResourceHandler("/webjars/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/");
+    }
 }
